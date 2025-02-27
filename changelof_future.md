bbb9dfb95e959ba76c8b8d7bdf818a821be388f7
Nikita Vdovin <narmunine@gmail.com>
2025-02-27 08:14:16 +0300

    27.02.25 changelog

---
+++ changelof_future.md
@@ -0,0 +1,10 @@
+**Done**:
+- Changed function calling logic
+- Changed anchor points
+- It's working with TM
+- It's stopped working with something else
+
+**Need to be done**
+- Change anchor points to \<main> tag
+- Add URL changing listener
+- Add feature, wich can understand is page scroleld at all
--- content_script.js
+++ content_script.js
@@ -1,46 +1,34 @@
-const current_url = String(document.URL);
+function get_hat() {
+	let hat = document.querySelector("div[data-id='main']");
+	if (!hat) {
+		setTimeout(
+			get_hat,
+			3000
+		);
+	} else {
+		console.log("[log] Probably, page is loaded at all");
 
-// Create a anchor 
-function create_an_anchor() {
-	let hat = document.getElementsByClassName("css-xg8uvt")[0];
-	let hat_child = hat.firstChild;
-	
-	console.log(hat_child);
-	
-	let new_div = document.createElement("div");
-	new_div.setAttribute("id", "new_anchor");
-	
-	hat.insertBefore(new_div, hat_child);
-	console.log("[log] Anchor was set");
-	
-	let bottom_part = document.getElementById("root");	
-	let first_child = bottom_part.firstChild;
-	let up_button_div = document.createElement("div");
-	up_button_div.setAttribute("class", "up_button_div");
-	
-	let up_button_a = document.createElement("a");
-	up_button_a.href = "#new_anchor";
-	up_button_a.setAttribute("class", "up_button_a");
-	up_button_a.textContent = "UP!";
-	
-	bottom_part.insertBefore(up_button_div, first_child);
-	up_button_div.appendChild(up_button_a);
+		// Create an anchor in a top part of page
+		let first_child = hat.firstChild;
 
-	console.log("[log] Anchor set");
-};
+		let anchor_div = document.createElement("div");
+		anchor_div.setAttribute("id", "anchor-up");
 
+		hat.insertBefore(anchor_div, first_child);
+		console.log("[log] Anchor was set");
 
-if (
-	(
-		current_url.includes("infomaximum") ||
-		current_url.includes("proceset") ||
-		current_url.includes("portal")
-	) && 
-		current_url.includes("publish")
-	) {
-	setTimeout(() => {
-		console.log("[log] Waited");
-		create_an_anchor();
-	}, 10000);
-	
-};
+		// Create a button fixed in bottom part of page
+		let button_up_div = document.createElement("div");
+		button_up_div.setAttribute("class", "up_button_div");
+
+		let button_up_a = document.createElement("a");
+		button_up_a.setAttribute("class", "up_button_a");
+		button_up_a.setAttribute("href", "#anchor-up");
+		button_up_a.textContent = "↑↑↑";
+
+		button_up_div.appendChild(button_up_a);
+		hat.appendChild(button_up_div);
+	};
+}
+
+let hat = get_hat();
